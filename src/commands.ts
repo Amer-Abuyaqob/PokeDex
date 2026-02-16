8de196612131bbb5e9f3a99c0cc2dbc080134d02
Amer-Abuyaqob <a.abwycwb@gmail.com>
2026-02-16 13:01:59 +0200

    Added 'help' command to Pokedex CLI in commands.ts for displaying help messages.

--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,6 @@
 import { CLICommand } from "./command.js";
 import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
 
 /**
  * Builds and returns the set of available Pokedex CLI commands.
@@ -15,6 +16,12 @@ export function getCommands(): Record<string, CLICommand> {
       name: "exit",
       description: "Exits the pokedex",
       callback: commandExit,
+    },
+
+    help: {
+      name: "help",
+      description: "Displays a help message",
+      callback: commandHelp,
     },
     // TODO: Add more commands (e.g. help, map, explore, catch, inspect, pokedex).
   };
