29fc135cd52b97becf9246b8b9d108f1993b6562
Amer-Abuyaqob <a.abwycwb@gmail.com>
2026-02-16 23:48:45 +0200

    Updated import statement in commands.ts to use type import for CLICommand.

--- src/commands.ts
+++ src/commands.ts
@@ -1,4 +1,4 @@
-import { CLICommand } from "./command.js";
+import type { CLICommand } from "./command.js";
 import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
 
@@ -23,6 +23,8 @@ export function getCommands(): Record<string, CLICommand> {
       description: "Displays a help message",
       callback: commandHelp,
     },
+    
     // TODO: Add more commands (e.g. help, map, explore, catch, inspect, pokedex).
+
   };
 }
