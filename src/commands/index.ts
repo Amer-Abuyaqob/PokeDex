d44de88b64909bbf27d2c7f854e8b22d8ee9bb93
Amer-Abuyaqob <a.abwycwb@gmail.com>
2026-02-24 04:05:38 +0200

    Added command exports in `src/commands/index.ts` for modular command management

---
+++ src/commands/index.ts
@@ -0,0 +1,8 @@
+export { commandHelp } from "./help.js";
+export { commandExit } from "./exit.js";
+export { commandMap } from "./map.js";
+export { commandMapb } from "./mapb.js";
+export { commandExplore } from "./explore.js";
+export { commandCatch } from "./catch.js";
+export { commandInspect } from "./inspect.js";
+export { commandPokeDex } from "./pokedex.js";
